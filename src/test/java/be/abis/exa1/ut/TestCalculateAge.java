a0e798a900c18400d4523690ebc209b45ca626e6
FortisTbo <mail4tbo@gmail.com>
2020-09-30 11:28:02 +0200

    exa4

---
+++ src/test/java/be/abis/exa1/ut/TestCalculateAge.java
@@ -0,0 +1,22 @@
+package be.abis.exa1.ut;
+
+import be.abis.exa1.model.Person;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+public class TestCalculateAge {
+
+    @Test
+    public void ageOfPersonFromBirthDateShouldBe42 () {
+        Person p1 = new Person(1,"Ann","Smits", LocalDate.of(1978, 6, 28));
+
+        Integer result = p1.calculateAge();
+
+        assertThat(result,is(equalTo(42)));
+    }
+}
