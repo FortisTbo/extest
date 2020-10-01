57ebd092054410fc348f40741dba6a588e2bb83f
FortisTbo <mail4tbo@gmail.com>
2020-10-01 09:58:53 +0200

    EXB3

---
+++ EXB1/src/test/java/ut/TestPerson.java
@@ -0,0 +1,52 @@
+package ut;
+
+import exb1.exception.MinorException;
+import exb1.model.Person;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestPerson {
+    private  Person p1;
+
+    @Before
+    public void setUp() {
+        p1 = new Person(1,"Ann","Smits", LocalDate.of(1978, 6, 28));
+
+    }
+
+    @Test
+    public void ageOfPersonFromBirthDateShouldBe42 () throws Exception {
+        Integer expected = 42;
+
+        Integer result = p1.calculateAge();
+        assertEquals (expected, result);
+    }
+
+    @Test
+    public void toStringSentenceStartsWithPerson () {
+
+        String p1ToString = p1.toString();
+
+        assertThat(p1ToString,startsWith("Person"));
+
+    }
+
+    @Test(expected= MinorException.class)
+    public void personShouldBeAdultException () throws Exception {
+        p1.setBirthDay(LocalDate.of(2010, 6, 28));
+
+        Integer result = p1.calculateAge();
+
+
+
+    }
+}
